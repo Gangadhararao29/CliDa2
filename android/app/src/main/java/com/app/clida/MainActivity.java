c0db837d92e55f6b0ad9c02a067b24e21f5eccd6
gangadhar <gangadhararao29@gmail.com>
2022-04-24 00:50:42 +0530

    splash added, spinner removed, icon zoomed

--- android/app/src/main/java/com/app/clida/MainActivity.java
+++ android/app/src/main/java/com/app/clida/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.app.clida;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,24 @@ const config: CapacitorConfig = {
   appName: 'CliDa',
   webDir: 'www',
   bundledWebRuntime: false,
+
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1500,
+      launchAutoHide: true,
+      backgroundColor: '#ffffffff',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      androidSpinnerStyle: 'large',
+      iosSpinnerStyle: 'small',
+      spinnerColor: '#999999',
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: 'launch_screen',
+      useDialog: false,
+    },
+  },
 };
 
 export default config;
