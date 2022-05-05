2b039494f7a6981612a8ee136415d6039770d5e1
chaga <gangadhararao29@gmail.com>
2022-05-05 18:34:24 +0530

    advSearch moved too dashboard & ui changes

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -22,14 +22,14 @@ const routes: Routes = [
           ),
       },
       {
-        path: 'clients-list/adv-search',
+        path: 'dashboard/adv-search',
         loadChildren: () =>
           import('../adv-search/adv-search.module').then(
             (m) => m.AdvSearchPageModule
           ),
       },
       {
-        path: 'clients-list/adv-search/client-details/:key',
+        path: 'dashboard/adv-search/client-details/:key',
         loadChildren: () =>
           import('../client-details/client-details.module').then(
             (m) => m.ClientDetailsPageModule
