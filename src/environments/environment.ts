56945dacf77cac2ca5995566242203880cba7d60
gangadhararao29 <gangadhararao29@gmail.com>
2023-05-28 14:28:54 +0530

    FE optmizations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBn17_kFv8LHIUXZRpZXILNrQKoM9utDHM',
+    authDomain: 'clida3.firebaseapp.com',
+    databaseURL:
+      'https://clida3-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'clida3',
+    storageBucket: 'clida3.appspot.com',
+    messagingSenderId: '967474760814',
+    appId: '1:967474760814:web:861e7b4043de9bb83e2a7f',
+    measurementId: 'G-F69J78TD5C',
+  },
 };
 
 /*
