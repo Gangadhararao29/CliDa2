f8c5c1985eef87d05c5b329e55fa674942ab57a6
ChaGa <gangadhararao29@gmail.com>
2024-11-16 01:11:59 +0530

    line chart update

--- src/app/dashboard/line-chart/line-chart.page.ts
+++ src/app/dashboard/line-chart/line-chart.page.ts
@@ -15,20 +15,11 @@ export class LineChartPage implements OnChanges {
     elements: { line: { tension: 0.5 } },
     locale: 'en-IN',
     scales: {
-      x: {},
-      interestScale: {
-        position: 'right',
-        ticks: {
-          color: 'green',
-          callback: (label: number) => label / 1000 + 'K',
-        },
-        grid: { drawOnChartArea: false },
-      },
       principalScale: {
         position: 'left',
         ticks: {
           color: '#36a2eb',
-          callback: (label: number) => label / 100000 + 'L',
+          callback: (label: number) => Math.round(label / 100000) + 'L',
         },
       },
     },
@@ -40,8 +31,8 @@ export class LineChartPage implements OnChanges {
   };
 
   labels = [];
-  dataSet1 = [0];
-  dataSet2 = [0];
+  dataSet1 = [];
+  dataSet2 = [];
   dataArray = [];
 
   constructor() {}
@@ -52,82 +43,104 @@ export class LineChartPage implements OnChanges {
       JSON.stringify(changes.responseData.previousValue)
     ) {
       this.labels = [];
-      this.dataSet1 = [0];
-      this.dataSet2 = [0];
+      this.dataSet1 = [];
+      this.dataSet2 = [];
       this.dataArray = [];
       this.groupByYear(changes.responseData.currentValue);
     }
   }
 
-  groupByYear(response) {
+  groupByYear(response, halfYearly = true) {
     const yearObject = {};
     response.forEach((record) => {
       record.data.data.forEach((ele) => {
         const startDateObj = new Date(ele.startDate);
         const year = startDateObj.getFullYear();
         const month = startDateObj.getMonth() + 1;
+
         if (!yearObject[year]) {
           yearObject[year] = [[], []];
         }
-        if (month <= 6) {
-          yearObject[year][0].push(ele);
+
+        if (halfYearly) {
+          if (month <= 6) {
+            yearObject[year][0].push(ele);
+          } else {
+            yearObject[year][1].push(ele);
+          }
         } else {
-          yearObject[year][1].push(ele);
+          yearObject[year][0].push(ele);
         }
       });
     });
-    this.calculateGraphData(yearObject);
+    this.calculateGraphData(yearObject, halfYearly);
   }
 
-  calculateGraphData(yearObject) {
+  calculateGraphData(yearObject, halfYearly) {
     const graphData = [];
     for (const fullYear in yearObject) {
       if (fullYear) {
         graphData[fullYear] = { prin1: 0, tot1: 0, prin2: 0, tot2: 0 };
         yearObject[fullYear][0].forEach((record) => {
-          graphData[fullYear].prin1 += record.principal;
-          graphData[fullYear].tot1 +=
-            (record.principal * record.interest) / 100.0;
+          if (record.principal > 0) {
+            graphData[fullYear].prin1 += record.principal;
+          } else {
+            graphData[fullYear].tot1 += Math.abs(record.principal);
+          }
         });
 
-        yearObject[fullYear][1].forEach((record) => {
-          graphData[fullYear].prin2 += record.principal;
-          graphData[fullYear].tot2 +=
-            (record.principal * record.interest) / 100.0;
-        });
+        if (halfYearly) {
+          yearObject[fullYear][1].forEach((record) => {
+            if (record.principal > 0) {
+              graphData[fullYear].prin2 += record.principal;
+            } else {
+              graphData[fullYear].tot2 += Math.abs(record.principal);
+            }
+          });
+        }
       }
     }
-    this.renderGraph(graphData);
+    this.renderGraph(graphData, halfYearly);
   }
 
-  renderGraph(graphData) {
-    Object.entries(graphData).forEach((ele: any) => {
-      this.labels.push(ele[0], ele[0].slice(2) + '-06');
-      this.dataSet1.push(ele[1].prin1, ele[1].prin2);
-      this.dataSet2.push(ele[1].tot1, ele[1].tot2);
-    });
-    this.labels.push(this.labels[this.labels.length - 2] * 1 + 1);
+  renderGraph(graphData, halfYearly) {
+    if (halfYearly) {
+      Object.entries(graphData).forEach((ele: any) => {
+        this.labels.push(ele[0], ele[0].slice(2) + '-06');
+        this.dataSet1.push(ele[1].prin1, ele[1].prin2);
+        this.dataSet2.push(ele[1].tot1, ele[1].tot2);
+      });
+    } else {
+      Object.entries(graphData).forEach((ele: any) => {
+        this.labels.push(ele[0]);
+        this.dataSet1.push(ele[1].prin1);
+        this.dataSet2.push(ele[1].tot1);
+      });
 
-    for (let i = 1; i < this.dataSet2.length; i++) {
-      this.dataSet2[i] += this.dataSet2[i - 1];
+    }
+
+    if (this.dataSet1[0] == 0 && this.dataSet2[0] == 0) {
+      this.dataSet1.shift();
+      this.dataSet2.shift();
+      this.labels.shift();
     }
 
     this.lineChartData = {
       datasets: [
         {
           data: this.dataSet1,
-          label: 'Principal',
+          label: 'Credits',
           yAxisID: 'principalScale',
-          backgroundColor: 'rgba(54, 163, 235, 0.712)',
+          backgroundColor: '#3f51b5',
           borderColor: '#36a2eb',
           pointBackgroundColor: '#36a2eb',
-          pointHoverBorderColor: '#36a2eb',
+          pointHoverBorderColor: '#6495ED',
         },
         {
           data: this.dataSet2,
-          label: 'Earnings',
-          yAxisID: 'interestScale',
-          backgroundColor: '#20ad2069',
+          label: 'Debits',
+          yAxisID: 'principalScale',
+          backgroundColor: '#dc3545',
           borderColor: 'green',
           pointBackgroundColor: 'green',
           pointHoverBorderColor: 'green',
